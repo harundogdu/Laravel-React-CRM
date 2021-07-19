98c9f3bdf3927f05a0c485bccf9f44583616e058
Harun Doğdu <Harundogdu06@gmail.com>
2021-07-19 20:22:18 +0300

    proje kurulumu yapıldı

---
+++ resources/js/Index.js
@@ -0,0 +1,13 @@
+import react, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Index extends Component {
+    render() {
+        return (
+            <div>Selamlar</div>
+        )
+    }
+}
+
+ReactDOM.render(<Index />, document.getElementById('index'));
+
